cda1177b5dd56b2719e136f8816487d123780d07
Max <maxuible@gmail.com>
2026-03-17 16:50:13 -0500

    day 23

---
+++ 268. Missing Number.py
@@ -0,0 +1,64 @@
+# Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+#
+#
+#
+# Example 1:
+#
+# Input: nums = [3,0,1]
+#
+# Output: 2
+#
+# Explanation:
+#
+# n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+#
+# Example 2:
+#
+# Input: nums = [0,1]
+#
+# Output: 2
+#
+# Explanation:
+#
+# n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+#
+# Example 3:
+#
+# Input: nums = [9,6,4,2,3,5,7,0,1]
+#
+# Output: 8
+#
+# Explanation:
+#
+# n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+
+from typing import List
+
+def missingNumber(nums: List[int]) -> int:
+    if len(nums) == 1:
+        if nums[0] == 1:
+            return 0
+        else:
+            return 1
+
+    nums_sorted = sorted(nums)
+
+
+
+    for i in range(len(nums_sorted) - 1):
+        if nums_sorted[i] + 1 == nums_sorted[i + 1]:
+            continue
+        else:
+            return nums_sorted[i] +1
+
+
+    if nums_sorted[-1] == len(nums_sorted):
+        return 0
+    else:
+        return len(nums_sorted)
+
+
+# print(missingNumber([3,0,1]))
+print(missingNumber([0,1]))
+print(missingNumber([1]))
+
