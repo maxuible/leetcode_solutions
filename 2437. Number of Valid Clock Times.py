95530901ffe53e53ff440a3e923632392ecf87b9
max <maxuible@gmail.com>
2026-04-21 16:09:54 -0500

    2026-04-21

---
+++ 2437. Number of Valid Clock Times.py
@@ -0,0 +1,53 @@
+# You are given a string of length 5 called time, representing the current time on a digital clock in the format "hh:mm". The earliest possible time is "00:00" and the latest possible time is "23:59".
+
+# In the string time, the digits represented by the ? symbol are unknown, and must be replaced with a digit from 0 to 9.
+
+# Return an integer answer, the number of valid clock times that can be created by replacing every ? with a digit from 0 to 9.
+
+ 
+
+# Example 1:
+
+# Input: time = "?5:00"
+# Output: 2
+# Explanation: We can replace the ? with either a 0 or 1, producing "05:00" or "15:00". Note that we cannot replace it with a 2, since the time "25:00" is invalid. In total, we have two choices.
+# Example 2:
+
+# Input: time = "0?:0?"
+# Output: 100
+# Explanation: Each ? can be replaced by any digit from 0 to 9, so we have 100 total choices.
+# Example 3:
+
+# Input: time = "??:??"
+# Output: 1440
+# Explanation: There are 24 possible choices for the hours, and 60 possible choices for the minutes. In total, we have 24 * 60 = 1440 choices.
+
+def countTime(time: str) -> int:
+    total = 1
+
+    if time[1] == "?" and time[0] == "?":
+        total *= 24
+    elif time[0] == "?":
+        if int(time[1]) >= 4:
+            total *= 2
+        else:
+            total *= 3
+    elif time[1] == "?":
+        if int(time[0]) >= 2:
+            total *= 4
+        else:
+            total *= 10
+
+    if time[3] == "?":
+        total *= 6
+
+    if time[4] == "?":
+        total *= 10
+
+    return total
+
+
+# print(countTime("?5:00"))
+
+# print(countTime("0?:0?"))
+print(countTime("??:??"))
