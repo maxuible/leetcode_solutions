8610d7dd1fd3de076d3634b86b49e2c61106bfa5
max <maxuible@gmail.com>
2026-04-25 21:11:18 -0500

    2026-04-25

---
+++ 2515. Shortest Distance to Target String in a Circular Array.py
@@ -0,0 +1,69 @@
+# You are given a 0-indexed circular string array words and a string target. A circular array means that the array's end connects to the array's beginning.
+
+# Formally, the next element of words[i] is words[(i + 1) % n] and the previous element of words[i] is words[(i - 1 + n) % n], where n is the length of words.
+# Starting from startIndex, you can move to either the next word or the previous word with 1 step at a time.
+
+# Return the shortest distance needed to reach the string target. If the string target does not exist in words, return -1.
+
+ 
+
+# Example 1:
+
+# Input: words = ["hello","i","am","leetcode","hello"], target = "hello", startIndex = 1
+# Output: 1
+# Explanation: We start from index 1 and can reach "hello" by
+# - moving 3 units to the right to reach index 4.
+# - moving 2 units to the left to reach index 4.
+# - moving 4 units to the right to reach index 0.
+# - moving 1 unit to the left to reach index 0.
+# The shortest distance to reach "hello" is 1.
+# Example 2:
+
+# Input: words = ["a","b","leetcode"], target = "leetcode", startIndex = 0
+# Output: 1
+# Explanation: We start from index 0 and can reach "leetcode" by
+# - moving 2 units to the right to reach index 2.
+# - moving 1 unit to the left to reach index 2.
+# The shortest distance to reach "leetcode" is 1.
+# Example 3:
+
+# Input: words = ["i","eat","leetcode"], target = "ate", startIndex = 0
+# Output: -1
+# Explanation: Since "ate" does not exist in words, we return -1.
+
+from typing import List
+
+def closestTarget(words: List[str], target: str, startIndex: int) -> int:
+    if target not in words:
+        return -1
+
+    closest = float('inf')
+
+    #forward
+    test = words[startIndex]
+    steps = 0
+    while test != target:
+        steps += 1
+        test = words[(startIndex + steps) % len(words)]
+
+    closest = steps
+
+
+    #back wards
+    steps = 0
+    test = words[startIndex]
+
+    while test != target:
+        steps += 1
+        test = words[(startIndex - steps) % len(words)]
+
+    if steps < closest:
+        closest = steps
+    
+    return closest
+
+
+# print(closestTarget(words = ["hello","i","am","leetcode","hello"], target = "hello", startIndex = 1))
+print(closestTarget(words = ["a","b","leetcode"], target = "leetcode", startIndex = 0))
+# print(closestTarget(words = ["hsdqinnoha","mqhskgeqzr","zemkwvqrww","zemkwvqrww","daljcrktje","fghofclnwp","djwdworyka","cxfpybanhd","fghofclnwp","fghofclnwp"], target = "zemkwvqrww", startIndex = 8))
+# print(closestTarget(words = ["i","eat","leetcode"], target = "ate", startIndex = 0))
