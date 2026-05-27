49bb908266259691c82ce9e06d92a3b4a0ae3afd
max <maxuible@gmail.com>
2026-05-27 04:54:31 -0500

    2026-05-27

---
+++ 2727. Is Object Empty.js
@@ -0,0 +1,13 @@
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+
+    if (obj.length > 0 || Object.keys(obj).length > 0) {
+        return false
+    } else {
+        return true
+    }
+
+};
